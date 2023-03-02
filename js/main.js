7ad3a298774cd69a233a4733bf4633328fc67d20
temurbek1995 <temurbek.alijoniy@yandex.com>
2023-03-02 09:00:46 +0300

    add changes

--- js/main.js
+++ js/main.js
@@ -9,3 +9,9 @@ const elHeaderBtn = elBody.querySelector('.header-btn')
 elHeaderBtn.addEventListener('click', function () {
   elBody.classList.toggle('dark-mode')
 })
+const elBtn = document.querySelector('.exit-btn')
+
+const elModal = document.querySelector('.modal')
+elBtn.addEventListener('click', function () {
+  elModal.classList.add('modal__close')
+})
